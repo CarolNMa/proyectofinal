8c6179b800fc7fdbd657ea3c6fc8c80d533bfad1
Carol <carolmarentes31@gmail.com>
2023-10-29 15:56:38 -0500

    Se agrego el index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <title>EMOJI COFFE BAR</title>
+        <link rel="stylesheet" href="../emoji/css/estilo.css">
+    </head>
+    <body class="body">
+        
+        <ul class="menu">
+            <li><a href="../emoji/html/Nosotros.php" >Nosotros</a></li>
+            <li><a href="../emoji/html/contacto.php">Contactos</a></li>
+            <li><a href="../emoji/html/log.php">Iniciar Sesion</a></li>
+
+            
+        </ul>
+        <div>
+            <img src="../emoji/imagenes/logo4.png"  class="imagen" alt="">
+        </div>    
+          
+        <div class="alerta">
+                    <p>Bienvenido a Emoji Coffe Shop Bar, es un gusto tenerte aqui, cada dia estamos
+                         mejorando para que tu experiencia y estadia sea genial en nuestro bar.</p>
+                </div>
+        
+                
+        
+    </body>
+
+    
+</html>
